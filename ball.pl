9062aec54bc0abd89b62badf9459bf593ae5a42c
Benjamin Landers <benjaminrlanders@gmail.com>
2013-09-05 13:05:02 -0700

    worked on parameter noting

--- ball.pl
+++ ball.pl
@@ -1,7 +1,18 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-my ($a, $v, $h, $t) =  @ARGV;
+sub search{
+	my $want = shift @_;
+	my @a = @_;
+	my $index = 0;
+	print $#a;
+	++$index until $a[$index] ne $want or $index > $#a;
+	return $index;
+}
+my ($a, $v, $h, $t) = (0,0,0,0); #  @ARGV;
+my $temp = &search("-v", @ARGV);
+print $temp;
+print "\n";
 for(my $i = 0; $i < $t; $i++){
 	print $i;
 	print ",";
