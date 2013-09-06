c4e3f0a27c149768741ccd861d4dae89b3984633
Benjamin Landers <benjaminrlanders@gmail.com>
2013-09-05 18:26:54 -0700

    made ball.pl work with -v's and such

--- ball.pl
+++ ball.pl
@@ -5,14 +5,24 @@ sub search{
 	my $want = shift @_;
 	my @a = @_;
 	my $index = 0;
-	print $#a;
-	++$index until $a[$index] ne $want or $index > $#a;
+	until($a[$index] eq $want or $index > $#a){
+		++$index;
+	}
+	if($index > $#a){
+		return -1;
+	}
 	return $index;
 }
-my ($a, $v, $h, $t) = (0,0,0,0); #  @ARGV;
-my $temp = &search("-v", @ARGV);
-print $temp;
-print "\n";
+#my ($a, $v, $h, $t) = (0,0,0,0); #  @ARGV;
+my $temp = &search("-a", @ARGV);
+my $a = $ARGV[$temp+1];
+$temp = &search("-v", @ARGV);
+my $v = $ARGV[$temp+1];
+$temp = &search("-h", @ARGV);
+my $h = $ARGV[$temp+1];
+$temp = &search("-t", @ARGV);
+my $t = $ARGV[$temp+1];
+
 for(my $i = 0; $i < $t; $i++){
 	print $i;
 	print ",";
