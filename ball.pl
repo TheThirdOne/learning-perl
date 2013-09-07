06d8132d1c26b0abcfb076db8131094d8fdad0b3
Benjamin Landers <benjaminrlanders@gmail.com>
2013-09-07 14:25:29 -0700

    reworked ball.pl to use tokenizer

--- ball.pl
+++ ball.pl
@@ -1,26 +1,16 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-sub search{
-	my $want = shift @_;
-	my @a = @_;
-	my $index = 0;
-	++$index until $#a < $index or $a[$index] eq $want;
-	if($index > $#a){
-		return -1;
-	}
-	return $index;
-}
-#my ($a, $v, $h, $t) = (0,0,0,0); #  @ARGV;
+use Tokenizer qw(tokenize);
+
 my ($a,$v,$h,$t) = (0,0,0,0);
-my $temp = &search("-a", @ARGV);
-$a = $ARGV[$temp+1] unless $temp == -1;
-$temp = &search("-v", @ARGV);
-$v = $ARGV[$temp+1] unless $temp == -1;
-$temp = &search("-h", @ARGV);
-$h = $ARGV[$temp+1] unless $temp == -1;
-$temp = &search("-t", @ARGV);
-$t = $ARGV[$temp+1] unless $temp == -1;
+my $inputs = tokenize @ARGV;
+
+$a = $inputs->{"-a"};
+$v = $inputs->{"-v"};
+$h = $inputs->{"-h"};
+$t = $inputs->{"-t"};
+
 print "T, V, H, A\n";
 for(my $i = 0; $i < $t; $i++){
 	print $i;
